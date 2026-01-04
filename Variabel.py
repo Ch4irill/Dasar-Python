2e74d1b491e535de6bdd55eb6bd1017559db489a
Chairil <dirgaakbar170805@gmail.com>
2026-01-05 00:29:09 +0900

    tambah file Variabel.py

---
+++ Variabel.py
@@ -0,0 +1,7 @@
+# VARIABEL DALAM PYTHON
+nama = 'Chairil'      # ini adalah string atau teks
+umur = 21             # ini adalah int atau angka bulat
+tinggi = 165.0        # ini adalah float atau angka desimal
+sudah_bekerja = True  # ini adalah bool True/False
+
+print(f'Perkenalkan namaku {nama} saya berumur {umur} lalu tinggi saya {tinggi} dan juga saya bekerja sebagai pramuniaga {sudah_bekerja}')
