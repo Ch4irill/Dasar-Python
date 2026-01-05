bfd22a210eab4bbfbf1697a091d35aa9f5011a44
Chairil <dirgaakbar170805@gmail.com>
2026-01-05 09:55:37 +0900

    meminta data dari pengguna

---
+++ Input.py
@@ -0,0 +1,10 @@
+# INPUT ADALAH DATA YANG DIBERIKAN PENGGUNA
+nama = input('Masukkan nama anda:   ')         # input untuk string
+print(f'Halo {nama}')
+
+umur = int(input('Masukkan umur anda:   '))    # int untuk angka desimal
+print(f'Kamu berumur {umur} tahun')
+
+
+tinggi = float(input('Masukkan tinggi anda:   ')) # float untuk angka desima
+print(f'tinggi mu {tinggi} cm')
