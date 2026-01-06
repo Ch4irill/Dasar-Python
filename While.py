a78c4a400945bced442c6b1da283b36375316934
Akbar <dirgaakbar170805@gmail.com>
2026-01-07 00:18:22 +0900

    Perulangan while

---
+++ While.py
@@ -0,0 +1,5 @@
+# WHILE ADALAH LOOP DENGAN SYARAT
+angka = 1		# Angkanya mulai dari 1
+while angka <= 5:	# Jika angkanya sama atau kurang dari 5 artinya True
+	print(angka)	
+	angka += 1	# Programnya tambah 1 hingga mencapai 5
