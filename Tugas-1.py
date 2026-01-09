05afeb3e365fa747a6aba89470fe6a2b6c10b365
Chairil <dirgaakbar170805@gmail.com>
2026-01-10 01:20:35 +0900

    Kalkulator

---
+++ Tugas-1.py
@@ -0,0 +1,17 @@
+# Program python dengan input membuat kalkulator sederhana
+angka1 = float(input('Angka Pertama:    '))
+operator = input('Pilih operator (+, -, *, /)    ')
+angka2 = float(input('Angka Kedua:    '))
+if operator == '+':
+    print(angka1 + angka2)
+
+elif operator == '-':
+    print(angka1 - angka2)
+
+elif operator == '*':
+    print(angka1 * angka2)
+
+elif operator == '/':
+    print(angka1 / angka2)
+
+
