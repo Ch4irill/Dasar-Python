df54961d3092768897e6bad44dc281e1f5893e34
Chairil <dirgaakbar170805@gmail.com>
2026-01-10 09:12:17 +0900

    Bug catcher

--- Tugas-2.py
+++ Tugas-2.py
@@ -1,8 +1,15 @@
 # Bug catcher
+#butterflies = 10
+#beetles = 12
+#ladybugs = 20
+#print('I caught ' + butterflies + ' 🦋 butterflies!')
+#print('I caught ' + beetles + ' 🪲 beetles!')
+#print('I caught ' + ladybug + ' 🐞 ladybugs!)
+#print('I caught ' + str(total) + ' total bugs!'
+
 butterflies = 10
 beetles = 12
 ladybugs = 20
-
 print(f'I caught {butterflies} 🦋 butterflies!')
 print(f'I caught {beetles} 🪲 beetles!')
 print(f'I caught {ladybugs} 🐞 ladybugs!')
