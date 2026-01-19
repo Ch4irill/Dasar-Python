13327bb045a12e63155dc772892cfe240c40918d
Chairil <dirgaakbar170805@gmail.com>
2026-01-19 21:14:40 +0900

    up

--- List.py
+++ List.py
@@ -6,4 +6,7 @@ print(buah[0]) # Menampilkan Apel
 print(buah[1])
 
 buah.append('anggur') # Menambahkan data di list terakhir
-buah.insert(1, 'nanas') # Menambahkan data di posisi tertentu 
+buah.insert(1, 'nanas') # Menambahkan data di posisi tertentu
+
+print(buah)
+print(len(buah))
