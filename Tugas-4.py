412b42594bbf2f7b6a82af50280f14e4f57e2e34
Chairil <dirgaakbar170805@gmail.com>
2026-01-21 13:07:00 +0900

    Masukkan pin

--- Tugas-4.py
+++ Tugas-4.py
@@ -1,6 +1,20 @@
 # Enter pin
+print('loops')
+
 pin = int(input('Masukkan pin atm kamu:	'))
 while pin != 1234:
-	pin = int(input('Pin salah, coaba lagi:	'))
+	pin = int(input('Pin salah, coba lagi:	'))
 
 print('pin diterima')
+# Enter pin dengan kesempatan
+print('3 kali percobaan')
+
+pin = int(input('Masukkan pin atm kamu:	'))
+kesempatan = 3
+while pin != 1234 and kesempatan > 0:
+	pin = int(input('Pin salah, coba lagi:	')) 
+	kesempatan -=1
+	if pin == 1234:
+		print('pin benar')
+	else:
+		print('Atm anda terblokir')
