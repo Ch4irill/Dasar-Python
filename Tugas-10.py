c6fda8099473ee1d4c7bbdbf69030591fed34488
Chairil <dirgaakbar170805@gmail.com>
2026-01-24 00:53:10 +0900

    FizBuz

---
+++ Tugas-10.py
@@ -0,0 +1,10 @@
+# Fizbuzz
+for angka in range(1, 20):
+    if angka % 3 == 0 and angka % 5 == 0:
+        print('FizzBuzz')
+    elif angka % 3 == 0:
+        print('Fizz')
+    elif angka % 5 == 0:
+        print('Buzz')
+    else:
+        print(angka)
