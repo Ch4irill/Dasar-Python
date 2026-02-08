1fcd41c18d8ce7ade1b19d3ba2667276b4a56d13
Dirga Akbar <dirgaakbar170805@gmail.com>
2026-02-08 21:16:49 +0900

    Menyimpan Kumpulan Data

--- 2_Variabel.py
+++ 2_Variabel.py
@@ -5,4 +5,3 @@ tinggi = 165.0        # ini adalah float atau angka desimal
 sudah_bekerja = True  # ini adalah bool True/False
 
 print(f'Perkenalkan namaku {nama} saya berumur {umur} lalu tinggi saya {tinggi} dan juga saya bekerja sebagai pramuniaga {sudah_bekerja}')
-
