4ce256e6808046f2bdfab2e7117557738eea556d
Chairil <dirgaakbar170805@gmail.com>
2026-02-13 00:19:56 +0900

    Program List

--- Tugas-11.py
+++ Tugas-11.py
@@ -7,3 +7,12 @@ toko = ['Telur',
         'Ayam']
 
 print(toko)
+print(toko[2 : 4]) # Menampilkan Kue & Bayam mulai dari 2 dan berhenti sebelum index 4
+
+toko.append('Labu') # Menambahkan di akhir list
+toko.insert(0, 'Nanas') # Menambahkan di posisi tertentu
+
+toko.remove('Ayam') # Menghapus dengan string
+toko.pop(1) # Menghapus dengan index
+
+print(toko)
